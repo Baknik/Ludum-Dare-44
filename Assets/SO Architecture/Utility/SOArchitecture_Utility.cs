9de38641dcc1111934a3901abf7ecbbdf9a14c86
Baknik <rks3159@gmail.com>
2019-04-26 11:32:16 -0600

    Initial Unity project setup

---
+++ Assets/SO Architecture/Utility/SOArchitecture_Utility.cs
@@ -0,0 +1,22 @@
+﻿public static class SOArchitecture_Utility
+{
+    public const int ASSET_MENU_ORDER_VARIABLES = 121;
+    public const int ASSET_MENU_ORDER_EVENTS = 122;
+    public const int ASSET_MENU_ORDER_COLLECTIONS = 123;
+    public const int ASSET_MENU_ORDER_CLAMPED_VARIABLES = 122;
+    public const int ASSET_MENU_ORDER_SYSTEMS = 124;
+    public const int ASSET_MENU_ORDER_RUNTIME_SETS = 125;
+
+    public const string VARIABLE_SUBMENU = "Variables/";
+    public const string VARIABLE_CLAMPED_SUBMENU = VARIABLE_SUBMENU + "Clamped/";
+    public const string COLLECTION_SUBMENU = "Collections/";
+    public const string GAME_EVENT = "Game Events/";
+    
+    public const string ADVANCED_GAME_EVENT = GAME_EVENT + "Advanced/";
+    public const string ADVANCED_VARIABLE_SUBMENU = VARIABLE_SUBMENU + "Advanced/";
+    public const string ADVANCED_VARIABLE_COLLECTION = COLLECTION_SUBMENU + "Advanced/";
+
+    public const string SYSTEM_SUBMENU = "Systems/";
+
+    public const string RUNTIME_SET_SUBMENU = "Runtime Sets/";
+}
